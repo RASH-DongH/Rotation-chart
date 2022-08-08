7a5a35a6b5fc58ba999cdc25f619ff2325335ba7
RASH.DongH <1696550530@qq.com>
2022-08-08 15:47:39 +0800

    轮播图封装完成

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue 3 + TypeScript + Vite
+
+# 本人自己封装的轮播图组件
+
+
+
+# 反正也是ts写的有数据类型提示  也可自行更改
+
+### 参数
++ sliders: 这是图片数据   对象数组  对象只有一个参数 imgUrl: 路径string类型，可自行更改
++ time: 多久换一张图   直接传如一个number值 即可
++ autoPlay: 是否自动播放  true 为自动播放  false  不播放
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Children from './Chilren.vue'
+const sliders = ref<{imgUrl: string}[]>([
+  {imgUrl: './assets/cos海琴烟 cosplay美女4k高清壁纸_彼岸图网.jpg'},
+  {imgUrl: './assets/dm.jpg'},
+  {imgUrl: './assets/hl.jpg'},
+  {imgUrl: './assets/iTab-1krxrv.jpg'},
+  {imgUrl: './assets/iTab-57jge1.jpg'},
+  {imgUrl: './assets/iTab-57o9j5.png'},
+  {imgUrl: './assets/iTab-6oe337.png'},
+  {imgUrl: './assets/iTab-72q3do.jpg'}
+])
+
+const time: number = 5000
+
+</script>
+
+<template>
+  轮播图
+  <hr />
+  <!-- 传一个time给子组件  来定义多少秒换一张图 -->
+  <Children :sliders="sliders"></Children>
+</template>
+
+<style scoped>
+
+</style>
